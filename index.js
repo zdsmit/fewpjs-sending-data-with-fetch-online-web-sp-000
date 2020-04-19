34be99621342b970bf0a0c8f06ee9b2b7dcc15c9
Zach  Smith <zdsmith001@gmail.com>
2020-04-19 16:05:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+function submitData(name, email) {
+  const configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name,
+      email
+    })
+  }
+
+  return fetch("http://localhost:3000/users", configObj)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(object) {
+    document.body.innerHTML = object["id"]
+  })
+  .catch(function(error) {
+    alert("Sorry, there was a problem!")
+    console.log(error.message)
+    document.body.innerHTML = error.message
+  })
+}
