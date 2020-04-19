ac240499a67d4c461c68c446438bb29ee570cb90
Zach  Smith <zdsmith001@gmail.com>
2020-04-19 15:37:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your code here
+function submitData(name, email) {
+  const configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name,
+      email
+    })
+  }
+
+  return fetch("http://localhost:3000/users", configObj)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(object) {
+    console.log(object);
+  })
+}
